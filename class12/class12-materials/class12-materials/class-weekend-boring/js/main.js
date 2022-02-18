a755c6980745a82d9248b9efedcfc0390eb0715b
Eds-Dbug <83380248+Eds-Dbug@users.noreply.github.com>
2022-02-17 23:23:25 -0500

    Started JS with Leon

--- class12/class12-materials/class12-materials/class-weekend-boring/js/main.js
+++ class12/class12-materials/class12-materials/class-weekend-boring/js/main.js
@@ -1,10 +1,15 @@
-document.querySelector('#check').addEventListener('click', check)
+document.querySelector('#check').addEventListener('click', check);
 
 function check() {
+	const day = document.querySelector('#day').value;
 
-  const day = document.querySelector('#day').value
+	//Conditionals go here
 
-  //Conditionals go here
-
-
+	if (day === 'Saturday' || day === 'Sun') {
+		document.querySelector('#placeToSee').innerText = 'Weekend';
+	} else if (day === 'Tuesday' || day === 'Thursday') {
+		document.querySelector('#placeToSee').innerText = 'Class Day';
+	} else {
+		document.querySelector('#placeToSee').innerText = 'boring day';
+	}
 }
