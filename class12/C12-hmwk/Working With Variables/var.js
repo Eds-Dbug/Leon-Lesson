8616e6a43d18d75fc097d4451926082095bce072
Eds-Dbug <83380248+Eds-Dbug@users.noreply.github.com>
2022-02-21 18:56:44 -0500

    Finished reading functions

--- class12/C12-hmwk/Working With Variables/var.js
+++ class12/C12-hmwk/Working With Variables/var.js
@@ -1,1 +1,34 @@
-let ourPlanet, userCurrent;
+let userInputPrompt;
+
+function pow(a, b) {
+	let result = a;
+	for (let i = 1; i < b; i++) {
+		result *= a;
+	}
+
+	return result;
+}
+
+while (userInputPrompt !== 'c') {
+	userInputPrompt = prompt('x pow y = ?');
+
+	let regex = /\d*\s\d*/g;
+	let savedInput = userInputPrompt.match(regex);
+	console.log('saved input: ' + savedInput[0]);
+	let twoNumbers = savedInput[0].split(' ');
+	//alert('two numbers: ' + twoNumbers[0] + ' ' + twoNumbers[1]);
+	let firstInput = twoNumbers[0],
+		secondInput = twoNumbers[1];
+
+	alert('? = : ' + pow(firstInput, secondInput));
+}
+
+// let regex = /[0-9]*\s[0-9]+/g;
+// let twoNumbers = userInputPrompt.match(regex);
+
+// alert(twoNumbers[0][1]);
+
+// // let firstInput = twoNumbers[0][0],
+// // 	secondInput = twoNumbers[0][1];
+
+// // alert('The pow is: ' + pow(firstInput, secondInput));
