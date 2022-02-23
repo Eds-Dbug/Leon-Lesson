e98434124ec86354650aeefd397e48a30230870e
Eds-Dbug <83380248+Eds-Dbug@users.noreply.github.com>
2022-02-22 21:41:10 -0500

    Class 13 excercises.

--- class13/class13-materials/class-weekend-boring/js/main.js
+++ class13/class13-materials/class-weekend-boring/js/main.js
@@ -1,17 +1,17 @@
 //Handle Capital Letters, Place result in DOM, add a check for humpday (Wed)
 
-document.querySelector('#check').addEventListener('click', check)
+document.querySelector('#check').addEventListener('click', check);
 
 function check() {
+	const day = document.querySelector('#day').value.toLowerCase();
 
-  const day = document.querySelector('#day').value
-
-  if(day === "tuesday" || day === "thursday"){
-    console.log("YOU HAVE CLASS")
-  }else if( day === "saturday" || day === "sunday"){
-    console.log("Its The Weekend")
-  }else{
-    console.log("BORING")
-  }
-
+	if (day === 'tuesday' || day === 'thursday') {
+		document.querySelector('#placeToSee').innerText = 'YOU HAVE CLASS';
+	} else if (day === 'saturday' || day === 'sunday') {
+		console.log('Its The Weekend');
+		document.querySelector('#placeToSee').innerText = 'Its The Weekend';
+	} else {
+		console.log('BORING');
+		document.querySelector('#placeToSee').innerText = 'BORING';
+	}
 }
--- class13/class13-materials/temperature-converter/js/main.js
+++ class13/class13-materials/temperature-converter/js/main.js
@@ -1,1 +1,10 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+
+document.querySelector('#yell').addEventListener('click', celciusToFarenheit);
+function celciusToFarenheit(celcius) {
+	//return conversionFormula(celcius);
+	let temp = document.querySelector('#sel').ariaValueMax;
+	temp = (temp * 9) / 5 + 32;
+}
+
+document.querySelector('#placeToYell').innerText = temp;
